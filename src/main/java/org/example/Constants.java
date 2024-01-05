170b0634688c669c4546f662a96ba26bfe320354
vladdumitru46 <vladdumitru46@yahoo.com>
2024-01-05 21:11:06 +0200

    am dat push

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Constants {
+
+    private Constants() {
+
+    }
+
+
+    public static List<Participant> participantsList = new ArrayList<>();
+    public static MyBlockingQueue myBlockingQueue = new MyBlockingQueue();
+
+    public static MyList<Participant> ranking = new MyList<>();
+
+    public static Set<Long> disqualifiedCompetitors = new HashSet<>();
+
+    public static AtomicInteger counter = new AtomicInteger(0);
+    public static ArrayList<String> serializableSubList = new ArrayList<>();
+
+    public static final AtomicLong lastUpdateTime = new AtomicLong(0);
+
+    public static String CountryRankingFile = "C:\\Users\\vladb\\Desktop\\Facultate\\an3\\programare paralela si distributiva\\java\\lab6\\src\\main\\resources\\out\\CountryRanking.txt";
+    public static String ParticipantRankingFile = "C:\\Users\\vladb\\Desktop\\Facultate\\an3\\programare paralela si distributiva\\java\\lab6\\src\\main\\resources\\out\\ParticipantsRanking.txt";
+
+}
